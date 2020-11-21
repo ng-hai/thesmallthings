63dc546d6bdf48129da78b29a3115a394e5c99fa
Hai Nguyen <ng-hai@Hais-MacBook-Air.local>
2020-11-21 17:39:37 +0700

    Get posts list at build time

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -7,15 +7,22 @@ import { importAll } from "utils"
 const Page = dynamic(() => import("components/page"), { ssr: false })
 const SEO = dynamic(() => import("components/seo"), { ssr: false })
 
-const postList = importAll(require.context(".", true, /.mdx?$/))
+export function getStaticProps() {
+  const postList = importAll(require.context(".", true, /.mdx?$/))
+  return {
+    props: {
+      posts: postList.map(({ meta }) => meta),
+    },
+  }
+}
 
-export default function BlogListing() {
+export default function BlogListing({ posts }) {
   return (
     <Page className="pb-10">
       <SEO title="Blog — The Small Things" />
       <div className="mx-auto max-w-line-length">
-        {postList.map(({ meta }, index) => (
-          <Link passHref href={meta.slug} key={index}>
+        {posts.map((meta) => (
+          <Link passHref href={meta.slug} key={meta.slug}>
             <a className="block mt-10 text-xl hover:cursor-pointer group">
               <time className="text-sm tracking-wide">{meta.date}</time>
               <h2 className="text-xl font-bold leading-tight tablet:text-2xl desktop:text-4xl group-hover:text-primary">
--- src/pages/projects.js
+++
@@ -1,5 +0,0 @@
-import Page from "../components/page"
-
-export default function Projects() {
-  return <Page />
-}
