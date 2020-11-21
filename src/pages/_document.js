13dafa1f30d040d9b7da8b4ac320071b4ebcbb5a
Hai Nguyen <ng-hai@Hais-MacBook-Air.local>
2020-11-21 16:09:52 +0700

    Inline Google Fonts

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -34,10 +34,6 @@ class MyDocument extends Document {
           <meta name="msapplication-TileColor" content="#1366E9" />
           <meta name="theme-color" content="#ffffff" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,400;0,600;0,700;1,400&family=IBM+Plex+Serif:wght@700&display=swap"
-            rel="stylesheet"
-          />
         </Head>
         <body>
           <Main />
